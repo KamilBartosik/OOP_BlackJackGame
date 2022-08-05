03c641fd0fa2dd365f5b7f54b0a5ba119e6d3a1b
Kamil Bartosik <bartosikkamil25@gmail.com>
2022-08-05 17:13:24 +0200

    rename display.py to functionalities.py and add new function

--- functionalities.py
+++ functionalities.py
@@ -14,3 +14,20 @@ def display(amount, balance, round_nb, player_cards, dealer_cards, values, state
         print('')
         print('Dealer cards: ', *dealer_cards, sep='\n')
         print(f'\nYour score is {values()[0]}! \nDealer score is {values()[-1]}!')
+
+
+def next_round():
+    next_rnd = ''
+    available_choices = ['yes', 'no']
+
+    while next_rnd not in available_choices:
+
+        next_rnd = input('Do you want to play next round (yes or no)?: ')
+
+        if next_rnd not in available_choices:
+            print("Please write your decision exactly as follows: 'yes' or 'no'")
+
+    if next_rnd == 'yes':
+        return True
+
+    return False
