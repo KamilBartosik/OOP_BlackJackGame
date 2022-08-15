fac7e0f5800073473be3b6df5df7ca819b0f0fdd
Kamil Bartosik <bartosikkamil25@gmail.com>
2022-08-15 18:45:43 +0200

    few fixes in functionalities.py and play.py

--- functionalities.py
+++ functionalities.py
@@ -7,26 +7,25 @@ def display(amount, balance, round_nb, player_cards, dealer_cards, values, state
 
     if state == 'player_round':
         print(f'\nYour cards: ', *player_cards, sep='\n')
-        print('')
-        print(f'Dealer cards: \n{dealer_cards[0]} \nUNKNOWN')
+        print(f'\nDealer cards: \n{dealer_cards[0]} \nUNKNOWN')
 
     else:
         print('\nYour cards: ', *player_cards, sep='\n')
-        print('')
-        print('Dealer cards: ', *dealer_cards, sep='\n')
+        print('\nDealer cards: ', *dealer_cards, sep='\n')
         print(f'\nYour score is {values()[0]}! \nDealer score is {values()[-1]}!')
 
 
 def next_round():
-    next_rnd = ''
-    available_choices = ['yes', 'no']
 
-    while next_rnd not in available_choices:
+    choices = ['yes', 'no']
 
+    while True:
         next_rnd = input('Do you want to play next round (yes or no)?: ')
 
-        if next_rnd not in available_choices:
-            print("Please write your decision exactly as follows: 'yes' or 'no'")
+        if next_rnd in choices:
+            break
+        else:
+            print("\nPlease write your decision exactly as follows: 'yes' or 'no'")
 
     if next_rnd == 'yes':
         return True
--- play.py
+++ play.py
@@ -13,47 +13,37 @@ def play_game(rules_func):
 
         print('\n' * 20 + '-' * 30)
         print('\nWelcome to Blackjack Game!')
-        balance = 500
         round_nb = 0
+        player = Player()
 
         while True:
 
             round_nb += 1
 
-            player = Player(balance)
             print(f'\n{player}')
 
             deck = Deck()
             deck.shuffle()
-
             player.place_bet()
-            bet = player.bet_amount
-            balance = player.balance
 
-            player_cards = []
-            dealer_cards = []
-
-            player_cards.append(deck.take_card())
-            dealer_cards.append(deck.take_card())
-            player_cards.append(deck.take_card())
-            dealer_cards.append(deck.take_card())
+            cards = [deck.take_card() for i in range(1, 5)]
+            player_cards = [cards[0], cards[2]]
+            dealer_cards = [cards[1], cards[3]]
 
             game = GameModules(deck, player_cards, dealer_cards)
 
-            display(bet, balance, round_nb, player_cards, dealer_cards, game.sum_value(), 'player_round')
+            display(player.bet_amount, player.balance, round_nb, player_cards, dealer_cards, game.sum_value(), 'player_round')
 
-            game.decision(bet, balance, round_nb)
-
+            game.decision(player.bet_amount, player.balance, round_nb)
             player.add_money(game.amount_of_bet)
-            balance = player.balance
 
-            if balance == 0:
+            if player.balance == 0:
                 print(f'\nYou lost all of your money!\n\nGAME OVER')
                 break
 
             if not next_round():
                 print(
-                    f'\nThanks for playing! You played {round_nb} rounds and end up with: {balance}$ ({balance - 500}$ profit)')
+                    f'\nThanks for playing! You played {round_nb} rounds and end up with: {player.balance}$ ({player.balance - 500}$ profit)')
                 break
 
 
